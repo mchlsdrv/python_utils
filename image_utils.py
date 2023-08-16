39f77b17d52f88e23d0f6616911857e1a1d214df
mchlsdrv <michael@nanoscout.com>
2023-08-16 15:13:57 +0000

    added image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,7 +1,16 @@
 import pathlib
+import numpy as np
 import cv2
 
 
-def get_image(image_file: str or pathlib.Path, flag=cv2.IMREAD_UNCHANGED):
+def get_image(image_file: str or pathlib.Path, to_gray: bool = False, flag=cv2.IMREAD_UNCHANGED):
     img = cv2.imread(str(image_file), flag)
+    try:
+        if to_gray:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = np.expand_dims(img, -1)  # Add channels dim
+        else:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    except Exception as err:
+        print(err)
     return img
